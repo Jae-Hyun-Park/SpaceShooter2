e305a26f9c4b0441cf0251825ae54e1b308237a8
asdf <asdf@naver.com>
2018-06-11 20:12:15 +0900

    2018.06.11

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -7,16 +7,39 @@ public class BarrelCtrl : MonoBehaviour {
     // 폴발 이펙트 프리팹
     public GameObject expEffect;
 
+    // 찌그러진 드럼통의 메쉬를 저장할 배열
+    public Mesh[] meshes;
+
+    // 드럼통의 텍스처들을 저장할 배열
+    public Texture[] textures;
     // 총알을 맞은 횟수
     int hitCount = 0;
 
     // Rigidbody Component 인스턴스
     Rigidbody rb;
+    
+    // 실제 렌더링 할 Mesh인스턴스
+    MeshFilter meshFilter;
+
+    // MeshFilter 컴포넌트 인스턴스
+    MeshRenderer meshRenderer;
 
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody>();	
-	}
+        
+        // 리지드바디 컴포넌트 인스턴스 연결
+        rb = GetComponent<Rigidbody>();
+
+        // 메쉬필터 컴포넌트 인스턴스 연결
+        meshFilter = GetComponent<MeshFilter>();
+        
+        // 메쉬 렌더러 컴포넌트 인스턴스 연결
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        // 메쉬 렌더러의 머터리얼이 참조하는 텍스쳐를 textures배열에서 랜덤하게 하나로 변경
+        meshRenderer.material.mainTexture = textures[Random.Range(0, textures.Length)];
+
+    }
 
     // 충돌 발생시 한번 호출되는 콜백 함수
     private void OnCollisionEnter(Collision collision)
@@ -40,7 +63,13 @@ public class BarrelCtrl : MonoBehaviour {
         rb.mass = 1.0f;
 
         Vector3 direction = Random.insideUnitSphere;
-        direction = new Vector3(direction.x, Mathf.Abs(direction.y), direction.z);
+        direction = new Vector3(Mathf.Clamp(direction.x, -0.5f, 0.5f),
+            1.0f,
+            Mathf.Clamp(direction.z, -0.5f, 0.5f));
+
         rb.AddForce(direction * 1000.0f);
+
+        // 0 ~ meshes 배열의 길이까지 (0 ~ meshes.Length - 1)
+        meshFilter.sharedMesh = meshes[Random.Range(0, meshes.Length)];
     }
 }
